b3fcbd62ef0918b7c4f7d962987aebcb3d065a41
Henry Haustein <henrydatei@web.de>
2021-11-08 21:25:33 +0100

    removed unnessesary packages

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-import requests
-import hashlib
-from requests.structures import CaseInsensitiveDict
 from api import BumbleAPI
 
-session = "s1:614:jl2NuxFIIs6IspMtqcWjtWNQWDHimSqJxFtEkWVT"
+session = ""
 
 api = BumbleAPI(session)
 for photo in api.getUser("zAhMACTc0NzA1NzY2NgjGPf5jAAAAACCUM4f3jmPyWNFLwU30cacn8dH_P4WgbdkzUftws9iF9w").photos:
