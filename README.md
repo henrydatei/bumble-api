ceab8e4c40d277782b58a8058804c3c84e4693c3
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-05-29 07:59:22 +0200

    better readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # bumble-api
 Reverse engineering the API of bumble.com
+
+# Usage
+- Clone the repository
+- Create a file in the repository
+- Run the following code in the file
+```python
+from api import BumbleAPI
+
+session = ""
+
+api = BumbleAPI(session)
+for searchResult in api.getMatchQueue():
+    print(searchResult.user.name)
+```
--- test.py
+++ test.py
@@ -3,7 +3,5 @@ from api import BumbleAPI
 session = ""
 
 api = BumbleAPI(session)
-for photo in api.getUser("zAhMACTc0NzA1NzY2NgjGPf5jAAAAACCUM4f3jmPyWNFLwU30cacn8dH_P4WgbdkzUftws9iF9w").photos:
-    print(photo.largeURL)
-
-api.getChat("zAhMACTc0NzA1NzY2NgjGPf5jAAAAACCUM4f3jmPyWNFLwU30cacn8dH_P4WgbdkzUftws9iF9w").printChat()
+for searchResult in api.getMatchQueue():
+    print(searchResult.user.name)
